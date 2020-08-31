8e7ab504efb0dfdf236fa100ab8ebc1910959bbb
muhamad safrudin abdul azis <safrudin2443420@gmail.com>
2020-08-31 20:12:37 +0700

    setup react routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Route>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Page Chackout</h1>
+          </Route>
+
+          <Route path="/login">
+            <h1>Page Login</h1>
+          </Route>
+
+          <Route path="/">
+            <h1>Home Page</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Route>
   );
 }
 
